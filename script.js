5755743c7afe83f77725ebeca299e6d16c15fd8f
ticopiccirillo <121138268+ticopiccirillo@users.noreply.github.com>
2024-03-19 17:43:34 -0300

    Tentativa de resposividade
    tudo

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnMobile = document.getElementById('btn-mobile') ;
+btnMobile.addEventListener('click' ,toggleMenu) ;
+btnMobile.addEventListener('Touchstart', toggleMenu) ;
+
+function toggleMenu( event) 
+{
+    if(event.type === 'touchstart') eventpreventdefaul
+const nav = document.getElementById('nav') ;
+nav.classList.toggle('active' )
+
+}
