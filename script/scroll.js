25e7741e6c858e63a9479afa499fa7d244f6b08c
ticopiccirillo <121138268+ticopiccirillo@users.noreply.github.com>
2024-03-27 17:05:14 -0300

    RESPOSIVIDADE
    AJUSTES

---
+++ script/scroll.js
@@ -0,0 +1,24 @@
+/*const menuItens = document.querySelectorAll('.nav a[href^="#"]');
+
+menuItens.forEach(item => {
+item.addEventListener('click', scrollToIdOnClick);
+});
+
+function scrollToIdOnClick(event) {
+     event.preventDefault();
+     
+     const to = getScrollTopByHref(event.target) - 150;
+     scroolToPosition(to);
+}
+
+function scrollToPosition(to) {
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    });
+}
+
+function getScrollTopByHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}*/
