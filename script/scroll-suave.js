0d134a3fc433deaa115e1d7ffc506ac77a8ef0e6
ticopiccirillo <121138268+ticopiccirillo@users.noreply.github.com>
2024-03-28 17:14:01 -0300

    atualização da atualização
    varias coisa.

--- script/scroll-suave.js
+++ script/scroll-suave.js
@@ -1,100 +1,42 @@
 ScrollReveal().reveal('.efeito-headline', { 
     duration: 3000,
-    distance: '300px', 
     delay: 200
      });
 ScrollReveal().reveal('.efeito-span', { 
-    duration: 3000,
-    distance: '300px', 
+    duration: 3000, 
     delay: 500
 });
 
 ScrollReveal().reveal('.efeito-boneca', { 
     duration: 3000,
-    distance: '300px', 
     delay: 800
 });
 
 ScrollReveal().reveal('.efeito-sobre', { 
     duration: 3000,
-    distance: '300px', 
     delay: 1000
  });
 
 ScrollReveal().reveal('.efeito-consult', 
 {  
     duration: 3000,
-    distance: '300px', 
     delay: 1000 
 });
 
 ScrollReveal().reveal('.efeito-aulas1', 
 {  
     duration: 3000,
-    distance: '300px', 
-    delay: 1500 
-});
-
-ScrollReveal().reveal('.efeito-aulas2', 
-{  
-    duration: 3000,
-    distance: '300px', 
-    delay: 1600 
-});
-
-ScrollReveal().reveal('.efeito-aulas3', 
-{  
-    duration: 3000,
-    distance: '300px', 
-    delay: 1700 
+    delay: 800 
 });
 
 ScrollReveal().reveal('.efeito-proj', 
 {  
-    duration: 3000,
-    distance: '300px', 
-    delay: 1500 
-});
-
-ScrollReveal().reveal('.efeito-proj1', 
-{  
-    duration: 3000,
-    distance: '300px', 
-    delay: 1600 
-});
-
-ScrollReveal().reveal('.efeito-proj2', 
-{  
-    duration: 3000,
-    distance: '300px', 
-    delay: 1700 
-});
-
-ScrollReveal().reveal('.efeito-proj3', 
-{  
-    duration: 3000,
-    distance: '300px', 
-    delay: 1800 
+    duration: 3000, 
+    delay: 800 
 });
 
 ScrollReveal().reveal('.efeito-depoi', 
 {  
     duration: 1500,
-    distance: '300px', 
-    delay: 1000 
+    delay: 800 
 });
-
-ScrollReveal().reveal('.efeito-depoi1', 
-{  
-    duration: 1600,
-    distance: '300px', 
-    delay: 3100 
-});
-
-ScrollReveal().reveal('.efeito-depoi2', 
-{  
-    duration: 1700,
-    distance: '300px', 
-    delay: 3500 
-});
-
