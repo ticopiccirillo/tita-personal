cd421b11ac4fb3422072ef9d5205dd6f7c5b0909
ticopiccirillo <121138268+ticopiccirillo@users.noreply.github.com>
2024-03-28 14:53:36 -0300

    Update scroll-suave.js
    Alteração de delay

--- script/scroll-suave.js
+++ script/scroll-suave.js
@@ -1,99 +1,99 @@
 ScrollReveal().reveal('.efeito-headline', { 
-    duration: 2000,
-    distance: '200px', 
-    delay: 500
+    duration: 3000,
+    distance: '300px', 
+    delay: 200
      });
 ScrollReveal().reveal('.efeito-span', { 
-    duration: 2500,
-    distance: '200px', 
+    duration: 3000,
+    distance: '300px', 
     delay: 500
 });
 
 ScrollReveal().reveal('.efeito-boneca', { 
-    duration: 2500,
-    distance: '250px', 
+    duration: 3000,
+    distance: '300px', 
     delay: 800
 });
 
 ScrollReveal().reveal('.efeito-sobre', { 
-    duration: 2000,
-    distance: '400px', 
+    duration: 3000,
+    distance: '300px', 
     delay: 1000
  });
 
 ScrollReveal().reveal('.efeito-consult', 
 {  
-    duration: 2000,
-    distance: '200px', 
+    duration: 3000,
+    distance: '300px', 
     delay: 1000 
 });
 
 ScrollReveal().reveal('.efeito-aulas1', 
 {  
-    duration: 2000,
+    duration: 3000,
     distance: '300px', 
     delay: 1500 
 });
 
 ScrollReveal().reveal('.efeito-aulas2', 
 {  
-    duration: 2000,
+    duration: 3000,
     distance: '300px', 
-    delay: 1700 
+    delay: 1600 
 });
 
 ScrollReveal().reveal('.efeito-aulas3', 
 {  
-    duration: 2000,
+    duration: 3000,
     distance: '300px', 
-    delay: 1900 
+    delay: 1700 
 });
 
 ScrollReveal().reveal('.efeito-proj', 
 {  
-    duration: 2000,
+    duration: 3000,
     distance: '300px', 
-    delay: 1900 
-});
-
-ScrollReveal().reveal('.efeito-proj3', 
-{  
-    duration: 2000,
-    distance: '300px', 
-    delay: 2000 
-});
-
-ScrollReveal().reveal('.efeito-proj2', 
-{  
-    duration: 2000,
-    distance: '300px', 
-    delay: 2200 
+    delay: 1500 
 });
 
 ScrollReveal().reveal('.efeito-proj1', 
 {  
-    duration: 2000,
+    duration: 3000,
     distance: '300px', 
-    delay: 2400 
+    delay: 1600 
+});
+
+ScrollReveal().reveal('.efeito-proj2', 
+{  
+    duration: 3000,
+    distance: '300px', 
+    delay: 1700 
+});
+
+ScrollReveal().reveal('.efeito-proj3', 
+{  
+    duration: 3000,
+    distance: '300px', 
+    delay: 1800 
 });
 
 ScrollReveal().reveal('.efeito-depoi', 
 {  
-    duration: 2000,
+    duration: 1500,
     distance: '300px', 
-    delay: 2900 
+    delay: 1000 
 });
 
 ScrollReveal().reveal('.efeito-depoi1', 
 {  
-    duration: 2000,
+    duration: 1600,
     distance: '300px', 
     delay: 3100 
 });
 
 ScrollReveal().reveal('.efeito-depoi2', 
 {  
-    duration: 2000,
+    duration: 1700,
     distance: '300px', 
     delay: 3500 
 });
